bc18a836e2a75eb42f31ba1b551aa1adfcc08f16
feng202511 <fengb600@gmail.com>
2024-11-06 13:58:46 +0800

    测试一下

--- 学习笔记/test.py
+++ 学习笔记/test.py
@@ -1,22 +1,19 @@
-import random
 
-def generate_code(code_len=4):
-    """
-    生成指定长度的验证码
+def main():
+    persons = [True] * 30
+    counter, index, number = 0, 0, 0
+    while counter < 15:
+        if persons[index]:
+            number += 1
+            if number == 9:
+                persons[index] = False
+                counter += 1
+                number = 0
+        index += 1
+        index %= 30
+    for person in persons:
+        print('基' if person else '非', end='')
 
-    :param code_len: 验证码的长度(默认4个字符)
 
-    :return: 由大小写英文字母和数字构成的随机验证码
-    """
-    all_chars = '0123456789abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ'
-    # 计算 all_chars 列表的最后一个元素的索引
-    last_pos = len(all_chars) - 1
-    # 此变量用于存储代码字符串
-    code = ''
-    for _ in range(code_len):
-        # 随机生成一个0到last_pos之间的整数
-        index = random.randint(0, last_pos)
-        # 将字符数组中的特定字符添加到代码字符串中
-        code += all_chars[index]
-    return code
-print(generate_code())
+if __name__ == '__main__':
+    main()
