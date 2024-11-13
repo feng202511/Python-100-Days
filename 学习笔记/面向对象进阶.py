01861dfe01c2d79a580c01c0478e36aa6df45acc
feng202511 <fengb600@gmail.com>
2024-11-13 16:12:15 +0800

    面向对象进阶：类与对象

---
+++ 学习笔记/面向对象进阶.py
@@ -0,0 +1,30 @@
+class Person:
+
+    def __init__(self,name,age):
+        self._name = name
+        self._age = age
+
+    def name(self):
+        return self._name
+    
+    def age(self):
+        return self._age
+    
+    def set_age(self, age):
+        self._age = age
+
+    def play(self):
+        if self._age <= 16:
+            print('%s的%s正在玩飞行棋.' % (self._age,self._name))
+        else:
+            print('%s的%s正在玩斗地主.' % (self._age,self._name))
+
+def main():
+    person = Person('王大锤', 12)
+    person.play()
+    person.set_age(22)
+    person.play()
+    # person.name = '白元芳'  # AttributeError: can't set attribute
+
+if __name__ == '__main__':
+    main()
